0b4a56c9b9a60f34b39816ea1794884da8838553
Marshall Ward <marshall.ward@gmail.com>
2014-06-01 18:27:16 +1000

    Matplotlib mostly complete

--- samples/sin_style.py
+++ samples/sin_style.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(0., 3*np.pi, 50)
 phase = np.arange(0., 2 * np.pi, 2 * np.pi / 3)
-style = ['r:', 'g--', 'py']
+#style = ['r:', 'g--', 'py']
+style = ['r--', 'bs', 'g^']
 
 for p, s in zip(phase, style):
     plt.plot(x, np.sin(x + p), s)
+
+plt.grid()
 plt.show()
